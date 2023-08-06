51c069fec9b3aa2925f3b750f8d5a5bdd14d11f8
Oshrat2812 <122016789+Oshrat2812@users.noreply.github.com>
2023-08-06 20:41:09 +0300

    oshrat-6.8

--- Group1-Node-Project/README.md
+++ Group1-Node-Project/README.md
@@ -1,1 +1,3 @@
 # NodeJS-Project
+
+MONGOOSE_URI = mongodb+srv://shalgony:XZl3FNYd6crPEEby@cluster0.d7by7a4.mongodb.net/
--- Group1-Node-Project/server.ts
+++ Group1-Node-Project/server.ts
@@ -26,19 +26,115 @@ app.use(express.static("./client"));
 
 app.use("/api/", companyRoute);
 
-// async function createAdmin(
-//   idNumber: number,
-//   name: string,
-//   password: string,
-//   email: string,
-//   phone: number
-// ) {
-//   const admin = new AdminModel({ idNumber, name, password, email, phone });
-//   const result = await admin.save();
-//   console.log(result);
-// }
+const roleSchema = new Schema({ name: String });
 
-// createAdmin(123456789, "admin", "123", "admin@gmail.com", 972506254875);
+const attendanceSchema = new Schema({
+  date: Date,
+  entry: Date,
+  exit: Date,
+});
+
+const employeeSchema = new Schema({
+  idNumber: Number,
+  name: String,
+  birthday: Date,
+  password: String,
+  email: String,
+  phone: Number,
+  salaryPerHour: Number,
+  role: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Role",
+  },
+  attendance: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Attendance",
+    },
+  ],
+});
+
+const managerSchema = new Schema({
+  idNumber: Number,
+  name: String,
+  birthday: Date,
+  password: String,
+  email: String,
+  phone: Number,
+  salaryPerHour: Number,
+  attendance: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Attendance",
+    },
+  ],
+  employees: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Employee",
+    },
+  ],
+});
+
+const adminSchema = new Schema({
+  idNumber: Number,
+  name: String,
+  birthday: Date,
+  password: String,
+  email: String,
+  phone: Number,
+  salaryPerHour: Number,
+  employees: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Employee",
+    },
+  ],
+});
+
+const companySchema = new Schema({
+  name: String,
+});
+
+// create collection
+const RoleModel = mongoose.model("Role", roleSchema);
+const EmployeeModel = mongoose.model("Employee", employeeSchema);
+const ManagerModel = mongoose.model("Manager", managerSchema);
+const AdminModel = mongoose.model("Admin", adminSchema);
+const CompanyModel = mongoose.model("Company", companySchema);
+const AttendanceModel = mongoose.model("Attendance", attendanceSchema);
+
+async function createAdmin(
+  idNumber: number,
+  name: string,
+  birthday: Date,
+  password: string,
+  email: string,
+  phone: number,
+  salaryPerHour: number
+) {
+  const admin = new AdminModel({
+    idNumber,
+    name,
+    birthday,
+    password,
+    email,
+    phone,
+    salaryPerHour,
+  });
+  const result = await admin.save();
+  console.log(result);
+}
+
+createAdmin(
+  123456789,
+  "admin",
+  new Date("1985-02-10"),
+  "123",
+  "admin@gmail.com",
+  972506254875,
+  50
+);
 
 app.listen(3000, () => {
   console.log("server listen on port 3000");
