2fffbad739b5d0e10b27e082bc8bee9c56f05246
Oshrat2812 <122016789+Oshrat2812@users.noreply.github.com>
2023-08-06 14:27:29 +0300

    Add files via upload

---
+++ API/dist/companyRoute.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var router = express_1["default"].Router();
+// import { addUsers, login, getUser } from "../API/usersControls";
+// router.post("/add-user", addUsers);
+// router.post("/login", login);
+// router.get("/get-user", getUser);
+exports["default"] = router;
