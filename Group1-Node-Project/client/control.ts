21e785d2b12c8f4364affd04be0a037979d3b65e
Ziv Vekstein <ziv.vek@gmail.com>
2023-08-08 08:20:50 +0300

    ziv shift allocation

--- Group1-Node-Project/client/control.ts
+++ Group1-Node-Project/client/control.ts
@@ -1,9 +1,32 @@
+let userType: UserType;
+
 enum UserType
 {
     Admin,
     Manager,
     Employee
 }
+
+async function getUserType() 
+{
+    try {
+        const response = await fetch("/api/admin/get-admin");
+        const data = await response.json();
+        console.log("data", data);
+        const {userDB } = data;
+        console.log("user: ", userDB);
+        
+        //if (response.ok === false) throw new Error("response from DB not ok");
+        
+        
+        
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
 
 const renderNavBar = (userType: UserType, navBarElem: HTMLDivElement) =>
 {
@@ -14,7 +37,15 @@ const renderNavBar = (userType: UserType, navBarElem: HTMLDivElement) =>
             
             break;
         case UserType.Manager:
-
+            const navBarHtml: string = `<div class="nav-bar__links-group">
+            <p class="nav-bar__link" onclick="gotoPage('../start-end-shift/employeeManager-HP.html')">Start / End Shift</p>
+            <p class="nav-bar__link nav-bar__link--bold">Shift Schedule</p>
+            <p class="nav-bar__link">Availability</p>
+            <p class="nav-bar__link">Employees</p>
+            <p class="nav-bar__link">Reports</p>
+            </div>
+            <p class="nav-bar__user-name">John Wick</p>`;
+            navBarElem.innerHTML = navBarHtml;
         break;
 
         case UserType.Employee:
@@ -23,27 +54,6 @@ const renderNavBar = (userType: UserType, navBarElem: HTMLDivElement) =>
     }
 }
 
-const renderNavBarStartEndShiftLink = () =>
-{
-
-}
-
-const renderNavBarShiftsScheduleLink = () =>
-{
-    
-}
-
-const renderNavBarAvailabilityLink = () =>
-{
-    
-}
-
-const renderNavBarEmployeesLink = () =>
-{
-    
-}
-
-const renderNavBarReportsLink = () =>
-{
-    
+const gotoPage = (targetPage: string) =>{
+    window.location.href = targetPage;
 }
--- Group1-Node-Project/client/shiftsAllocation/dist/shiftsAllocation.js
+++ Group1-Node-Project/client/shiftsAllocation/dist/shiftsAllocation.js
@@ -1,1 +1,2 @@
 renderNavBar(UserType.Manager, navBarElement);
+getUserType();
