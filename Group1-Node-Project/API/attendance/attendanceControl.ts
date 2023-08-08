c4d11f1fcd74e8e74a4c0190ef3eb4d0b7ceae3c
aliyah <aliyah1mesi@gmail.com>
2023-08-08 14:47:08 +0300

    aliyah

--- Group1-Node-Project/API/attendance/attendanceControl.ts
+++
@@ -1,6 +0,0 @@
-import AttendanceModel from "./attendanceModel";
-import ManagerModel from "../manager/managerModel";
-import RoleModel from "../role/roleModel";
-import EmployeeModel from "../employee/employeeModel";
-import CompanyModel from "../company/companyModel";
-import AdminModel from "../admin/adminModel";
