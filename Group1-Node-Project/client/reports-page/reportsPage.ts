3bb132ebf024b443b1019aac89267b995978c5d7
Oshrat2812 <122016789+Oshrat2812@users.noreply.github.com>
2023-08-09 20:46:14 +0300

    oshrat-9.8

---
+++ Group1-Node-Project/client/reports-page/reportsPage.ts
@@ -0,0 +1,65 @@
+const navBarElement = document.querySelector(".nav-bar") as HTMLDivElement;
+const userName = document.querySelector("#userName") as HTMLDivElement;
+
+const salaryButton = document.querySelector(
+  ".reportButtons__salary"
+) as HTMLDivElement;
+
+const managerButton = document.querySelector(
+  ".reportButtons__manager"
+) as HTMLDivElement;
+
+const employeeButton = document.querySelector(
+  ".reportButtons__employee"
+) as HTMLDivElement;
+
+const reportsBySalary = document.querySelector(
+  ".salaryReports"
+) as HTMLDivElement;
+
+// const reportsByManager = document.querySelector(
+//   ".salaryReports"
+// ) as HTMLDivElement;
+
+// const reportsByEmployee = document.querySelector(
+//   ".salaryReports"
+// ) as HTMLDivElement;
+
+const reportSalaryUp = document.querySelector(
+  ".salaryReports__salaryUp"
+) as HTMLDivElement;
+
+const reportSalaryDown = document.querySelector(
+  ".salaryReports__salaryDown"
+) as HTMLDivElement;
+
+const reportSalaryBetween = document.querySelector(
+  ".salaryReports__between"
+) as HTMLDivElement;
+
+async function main() {
+  await getActiveUser();
+
+  renderNavBar(navBarElement);
+  const userName: HTMLDivElement | null = document.querySelector("#userName");
+
+  if (!userName) throw new Error("No user element on DOM");
+  userName.innerText = user.name;
+}
+
+main();
+
+salaryButton.addEventListener("click", (e) => {
+  reportsBySalary.style.display = "flex";
+});
+
+function HandleSalaryUp(ev) {
+  try {
+    ev.preventDefault();
+    const salaryUp = ev.target.elements.salaryUp.value;
+    if (!salaryUp) throw new Error("no salary entered");
+    console.log(salaryUp);
+  } catch (error) {
+    console.log(error);
+  }
+}
