7d778569bf169077295c952b79f49480690260e2
Oshrat2812 <122016789+Oshrat2812@users.noreply.github.com>
2023-08-09 22:27:40 +0300

    oshrat-9.8

--- Group1-Node-Project/client/common/dist/dist/control.dev.js
+++ Group1-Node-Project/client/common/dist/dist/control.dev.js
@@ -144,6 +144,7 @@ var __generator = void 0 && (void 0).__generator || function (thisArg, body) {
 };
 
 var userType;
+var user;
 var UserType;
 
 (function (UserType) {
@@ -154,11 +155,11 @@ var UserType;
 
 function getActiveUser() {
   return __awaiter(this, void 0, void 0, function () {
-    var responseManager, dataManager, manager, responseAdmin, dataAdmin, adminDB, error_1;
+    var responseManager, dataManager, manager, error_1, responseAdmin, dataAdmin, admin, error_2, responseEmployee, dataEmployee, employee, error_3;
     return __generator(this, function (_a) {
       switch (_a.label) {
         case 0:
-          _a.trys.push([0, 5,, 6]);
+          _a.trys.push([0, 3,, 4]);
 
           return [4
           /*yield*/
@@ -177,30 +178,46 @@ function getActiveUser() {
           console.log("user: ", manager);
 
           if (dataManager.ok === true && manager._id !== null) {
-            userType = UserType.Employee;
+            userType = UserType.Manager;
+            user = manager;
             console.log("userType: ", userType);
             return [2
             /*return*/
             ];
           }
+
+          return [3
+          /*break*/
+          , 4];
+
+        case 3:
+          error_1 = _a.sent();
+          console.error(error_1);
+          return [3
+          /*break*/
+          , 4];
+
+        case 4:
+          _a.trys.push([4, 7,, 8]);
 
           return [4
           /*yield*/
           , fetch("/api/admin/get-admin")];
 
-        case 3:
+        case 5:
           responseAdmin = _a.sent();
           return [4
           /*yield*/
           , responseAdmin.json()];
 
-        case 4:
+        case 6:
           dataAdmin = _a.sent();
-          adminDB = dataAdmin.adminDB;
-          console.log("user: ", adminDB);
+          admin = dataAdmin.admin;
+          console.log("user: ", admin);
 
-          if (dataAdmin.ok === true && adminDB._id !== null) {
+          if (dataAdmin.ok === true && admin._id !== null) {
             userType = UserType.Admin;
+            user = admin;
             console.log("userType: ", userType);
             return [2
             /*return*/
@@ -209,16 +226,54 @@ function getActiveUser() {
 
           return [3
           /*break*/
-          , 6];
+          , 8];
 
-        case 5:
-          error_1 = _a.sent();
-          console.error(error_1);
+        case 7:
+          error_2 = _a.sent();
+          console.error(error_2);
           return [3
           /*break*/
-          , 6];
+          , 8];
 
-        case 6:
+        case 8:
+          _a.trys.push([8, 11,, 12]);
+
+          return [4
+          /*yield*/
+          , fetch("/api/employee/get-employee")];
+
+        case 9:
+          responseEmployee = _a.sent();
+          return [4
+          /*yield*/
+          , responseEmployee.json()];
+
+        case 10:
+          dataEmployee = _a.sent();
+          employee = dataEmployee.employee;
+          console.log("user: ", employee);
+
+          if (dataEmployee.ok === true && employee._id !== null) {
+            userType = UserType.Employee;
+            user = employee;
+            console.log("userType: ", userType);
+            return [2
+            /*return*/
+            ];
+          }
+
+          return [3
+          /*break*/
+          , 12];
+
+        case 11:
+          error_3 = _a.sent();
+          console.error(error_3);
+          return [3
+          /*break*/
+          , 12];
+
+        case 12:
           return [2
           /*return*/
           ];
@@ -227,20 +282,54 @@ function getActiveUser() {
   });
 }
 
-var renderNavBar = function renderNavBar(userType, navBarElem) {
-  console.log(userType, navBarElem);
+var renderNavBar = function renderNavBar(navBarElem) {
+  var targetDivEle;
+  if (!navBarElem) console.error("no nav bar HTMLDivElement received");
 
   switch (userType) {
     case UserType.Admin:
+      var navBarHtml = '<div class="nav-bar__links-group">\n                <p class="nav-bar__link nav-bar__link__shift-schedule" onclick="gotoPage(\'../shift-schedule-page/shiftSchedule.html\')">Shift Schedule</p>\n                <p class="nav-bar__link nav-bar__link__availability" onclick="gotoPage(\'../availability-page/availabilityPage.html\')">Availability</p>\n                <p class="nav-bar__link nav-bar__link__employees" onclick="gotoPage(\'../employee-list-page/employeePage.html\')">Employees</p>\n                <p class="nav-bar__link nav-bar__link__reports" onclick="gotoPage(\'../reports-page/reportsPage.html\')">Reports</p>\n                </div>';
+      navBarElem.innerHTML = navBarHtml;
       break;
 
     case UserType.Manager:
-      var navBarHtml = "<div class=\"nav-bar__links-group\">\n            <p class=\"nav-bar__link\" onclick=\"gotoPage('../start-end-shift/employeeManager-HP.html')\">Start / End Shift</p>\n            <p class=\"nav-bar__link nav-bar__link--bold\">Shift Schedule</p>\n            <p class=\"nav-bar__link\">Availability</p>\n            <p class=\"nav-bar__link\">Employees</p>\n            <p class=\"nav-bar__link\">Reports</p>\n            </div>\n            <p class=\"nav-bar__user-name\">John Wick</p>";
+      navBarHtml = '<div class="nav-bar__links-group">\n                <p class="nav-bar__link nav-bar__link__employee-manager" onclick="gotoPage(\'../managerHP/managerHP.html\')">Start / End Shift</p>\n                <p class="nav-bar__link nav-bar__link__shift-schedule" onclick="gotoPage(\'../shift-schedule-page/shiftSchedule.html\')">Shift Schedule</p>\n                <p class="nav-bar__link nav-bar__link__availability" onclick="gotoPage(\'../availability-page/availabilityPage.html\')">Availability</p>\n                <p class="nav-bar__link nav-bar__link__employees" onclick="gotoPage(\'../employee-list-page/employeePage.html\')">Employees</p>\n                <p class="nav-bar__link nav-bar__link__reports" onclick="gotoPage(\'../reports-page/reportsPage.html\')">Reports</p>\n                </div>';
       navBarElem.innerHTML = navBarHtml;
       break;
 
     case UserType.Employee:
+      navBarHtml = '<div class="nav-bar__links-group">\n                <p class="nav-bar__link nav-bar__link__employee-manager" onclick="gotoPage(\'../employeeHP/employeeHP.html\')">Start / End Shift</p>\n                <p class="nav-bar__link nav-bar__link__shift-schedule" onclick="gotoPage(\'../shift-schedule-page/shiftSchedule.html\')">Shift Schedule</p>\n                <p class="nav-bar__link nav-bar__link__availability" onclick="gotoPage(\'../availability-page/availabilityPage.html\')">Availability</p>\n                <p class="nav-bar__link nav-bar__link__employees" onclick="gotoPage(\'../employee-list-page/employeePage.html\')">Employees</p>\n                <p class="nav-bar__link nav-bar__link__reports" onclick="gotoPage(\'../reports-page/reportsPage.html\')">Reports</p>\n                </div>';
+      navBarElem.innerHTML = navBarHtml;
+  }
+
+  switch (window.location.pathname) {
+    case "/start-end-shift/employeeManager-HP.html":
+      targetDivEle = document.querySelector(".nav-bar__link__employee-manager");
       break;
+
+    case "/shift-schedule-page/shiftSchedule.html":
+      targetDivEle = document.querySelector(".nav-bar__link__shift-schedule");
+      break;
+
+    case "/availability-page/availabilityPage.html":
+      targetDivEle = document.querySelector(".nav-bar__link__availability");
+      break;
+
+    case "/employees-page/employeesPage.html":
+      targetDivEle = document.querySelector(".nav-bar__link__employees");
+      break;
+
+    case "/reports-page/reportsPage.html":
+      targetDivEle = document.querySelector(".nav-bar__link__reports");
+      break;
+
+    default:
+      console.error("No location.pathname found.");
+      break;
+  }
+
+  if (targetDivEle) {
+    targetDivEle.classList.add("nav-bar__link--bold");
   }
 };
 
