2e468c2205e23ec40a393a8dad8b0b870f0eac30
aliyah <aliyah1mesi@gmail.com>
2023-08-09 18:02:08 +0300

    aliyah

--- Group1-Node-Project/API/availability/dist/availabilityModel.js
+++ Group1-Node-Project/API/availability/dist/availabilityModel.js
@@ -3,13 +3,13 @@ exports.__esModule = true;
 exports.WeekModel = void 0;
 var mongoose_1 = require("mongoose");
 var weekSchema = new mongoose_1.Schema({
-    sunday: [],
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: [],
-    saturday: [],
+    Sunday: [],
+    Monday: [],
+    Tuesday: [],
+    Wednesday: [],
+    Thursday: [],
+    Friday: [],
+    Saturday: [],
     comment: []
 });
 exports.WeekModel = mongoose_1["default"].model("Week", weekSchema);
